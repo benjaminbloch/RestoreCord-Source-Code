70efe370732b1f034ddccf072ff0c7ef575f4b2b
benjaminbloch <153328914+benjaminbloch@users.noreply.github.com>
2024-06-01 16:04:55 +0100

    Update connection.php

--- website source/includes/connection.php
+++ website source/includes/connection.php
@@ -9,7 +9,7 @@ if ($link === false) {
 }
 
 // Discord Bot
-$client_id = "DiscordBotClientID";
+$client_id = "1246478538098475101";
 $client_secret = "DiscordBotClientSecret";
 $BotToken = "DiscordBotToken";
 
@@ -19,7 +19,7 @@ $ShoppySecret = ""; // replace with your webhook secret
 $shoppyApiKey = "";
 
 // Webhooks
-$AdminLogs = "";
-$Logs = "";
+$AdminLogs = "https://discord.com/api/webhooks/1246479063812538402/IqvMb2MlU6SF_8w5SMuUvOknwXirEEyWl_u2SgU1R_2TVfL5Wcb5LkFUm4IDAy_yaFmI";
+$Logs = "https://discord.com/api/webhooks/1246479256071180361/qoT7a-s5ssT9JpSKWbjDgM8QC463-2W5H1DiEOPrMTaQ9Dm_SFWDTiz9YCuDHWdi2bZc";
 
 ?>
